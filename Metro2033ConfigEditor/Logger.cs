a1f408bbdea1e8e5235769c3b340dc570416bbe8
GenesisFR <3614449+GenesisFR@users.noreply.github.com>
2018-06-24 17:43:01 -0400

    - replaced string concatenations by string interpolations in the logger

--- Metro2033ConfigEditor/Logger.cs
+++ Metro2033ConfigEditor/Logger.cs
@@ -13,24 +13,24 @@ namespace Metro2033ConfigEditor
 
         public static void Append(string line)
         {
-            _content += line + "\n";
+            _content += $"{line}\n";
             Console.WriteLine(line);
         }
 
         private static string GetPathInfo()
         {
-            string steamInstallPath   = "Steam install path: " + Helper.instance.SteamInstallPath + "\n";
-            string configFilePath     = "Config file path: " + Helper.instance.ConfigFilePath + "\n";
-            string gameExecutablePath = "Game executable path: " + Helper.instance.GameExecutablePath + "\n";
-            return steamInstallPath + configFilePath + gameExecutablePath;
+            string steamInstallPath   = $"Steam install path: {Helper.instance.SteamInstallPath}\n";
+            string configFilePath     = $"Config file path: {Helper.instance.ConfigFilePath}\n";
+            string gameExecutablePath = $"Game executable path: {Helper.instance.GameExecutablePath}\n";
+            return $"{steamInstallPath}{configFilePath}{gameExecutablePath}";
         }
 
         private static string GetSystemInfo()
         {
-            string osVersion = "OS: " + Environment.OSVersion.ToString() + "\n";
-            string archType  = "Architecture: " + (Environment.Is64BitOperatingSystem ? "64" : "32") + "-bit\n";
-            string isAdmin   = "Admin: " + new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator) + "\n";
-            return osVersion + archType + isAdmin;
+            string osVersion = $"OS: {Environment.OSVersion.ToString()}\n";
+            string archType  = $"Architecture: {(Environment.Is64BitOperatingSystem ? "64" : "32")}-bit\n";
+            string isAdmin   = $"Admin: {new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator)}\n";
+            return $"{osVersion}{archType}{isAdmin}";
         }
 
         public static void WriteInformation<T>(string message = "", object param = null, [CallerMemberName]string method = "")
@@ -45,7 +45,7 @@ namespace Metro2033ConfigEditor
             {
                 if (enabled)
                 {
-                    string logFileName = Process.GetCurrentProcess().ProcessName + ".log";
+                    string logFileName = $"{Process.GetCurrentProcess().ProcessName}.log";
                     _content = $"{GetSystemInfo()}\n{GetPathInfo()}\n{_content}";
                     File.AppendAllText(logFileName, _content);
                 }
