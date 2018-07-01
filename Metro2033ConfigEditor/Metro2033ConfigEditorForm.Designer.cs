c5adc8c002f68b266b8b3d2438f7c7c1a4624728
GenesisFR <3614449+GenesisFR@users.noreply.github.com>
2018-07-01 12:47:40 -0400

    - updated version number

--- Metro2033ConfigEditor/Metro2033ConfigEditorForm.Designer.cs
+++ Metro2033ConfigEditor/Metro2033ConfigEditorForm.Designer.cs
@@ -1735,7 +1735,7 @@
             this.MaximizeBox = false;
             this.Name = "Metro2033ConfigEditorForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Metro 2033 Config Editor v1.31";
+            this.Text = "Metro 2033 Config Editor v1.32";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Metro2033ConfigEditorForm_Closing);
             this.Shown += new System.EventHandler(this.Metro2033ConfigEditorForm_Shown);
             this.tabVideo.ResumeLayout(false);
